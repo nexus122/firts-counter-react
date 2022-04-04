f0ee1e03dff0a5c395e6022df0444a42f47fd84f
nexus122 <juan-pablo122@hotmail.es>
2022-04-04 13:35:16 +0200

    Nueva app contador

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CounterBox></CounterBox>
     </div>
   );
 }
 
+function CounterBox() {
+
+  const [counter, setCounter] = useState(0); // Esto se crea con un array, no con un objeto.
+  const [color, setColor] = useState('red');
+
+  /*function changeCounter(value) {
+    console.log("Cambiamos el contador: ", value);
+    setCounter(counter + + value);
+
+    let prevText = counter + + value;
+    
+    // Actualizamos la variable de color.
+    if (prevText < 0) {
+      setColor('red')
+    } else if (prevText >= 1) {
+      setColor('green')
+    } else {
+      setColor('black')
+    }
+    
+  }*/
+
+  function changeCounter(value){
+    setCounter(()=>{
+      const newColor = counter + + value > 0? 'green':'red';
+      setColor(newColor);
+      return counter + + value;
+    })
+  }
+
+  return (
+    <div className='CounterBox'>
+      <h1>Contador</h1>
+
+      <div className='CounterValue'>
+        <span style={{'color': color}}>{counter}</span>
+      </div>
+
+      <div className="BtnContainer">
+        <Button updateValue={changeCounter} text="Lower Count" value="-1"></Button>
+        <Button updateValue={changeCounter} text="Add Count" value="1"></Button>
+      </div>
+    </div>
+  )
+}
+
+function Button(props) {
+
+  // Hay que crear una funcion para gestrionar la llamada al padre.
+  // Sirve para identificar el valor y/o el btn pulsado, permite utilizar funciones
+  // y vincularlas a eventos.
+  function handleClick() {
+    props.updateValue(props.value);
+  }
+
+  // Dibujamos el boton.
+  return (
+    <button
+      onClick={handleClick}
+      value={props.value}
+      className='Btn'>{props.text}</button>
+  )
+
+}
 export default App;
